e1a7bbdd43acd25df0b18bc14c376d9992bd14ec
curious-metric-5246 <richardjharvey1@gmail.com>
2020-04-23 18:34:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function theBeatlesPlay(musicians, instruments){
   for(let i = 0; i < musicians.length; i++){
     push.musiciansArray(musicians[i] + " plays " + instruments[i]);
   }
-  
+  return musiciansArray;
   
 }
 
