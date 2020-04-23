13665b0e6169a87b99e6093840022843620b3693
curious-metric-5246 <richardjharvey1@gmail.com>
2020-04-23 18:29:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 // add solution here
-function theBeatlesPlay(musicians, instruments){}
+
+
+function theBeatlesPlay(musicians, instruments){
+  var musiciansArray = [];
+  for(let i = 0; i < musicians.legth; i++){
+    console.log(musicians[i] + " plays " + instruments[i])
+  }
+  
+  
+}
+
+theBeatlesPlay();
